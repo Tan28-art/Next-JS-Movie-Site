b26a996a7bc7bd135daa5658bd88a480173b18bd
Tan28-art <tanmish03@gmail.com>
2024-07-30 17:49:24 +0300

    Fixed bug with error button

--- starter/app/error.tsx
+++ starter/app/error.tsx
@@ -14,7 +14,7 @@ export default function App({ error, reset }: IAppProps) {
   return (
     <div className = 'text-center mt-20'>
       <h1>Something went wrong.</h1>
-      <button className="border-2 border-dashed rounded-3xl p-1 px-2 mt-5 border-black dark:border-white dark:hover:border-emerald-500 hover:border-emerald-500 hover:text-emerald-500 " onClick={() => reset}>
+      <button className="border-2 border-dashed rounded-3xl p-1 px-2 mt-5 border-black dark:border-white dark:hover:border-emerald-500 hover:border-emerald-500 hover:text-emerald-500 " onClick={() => reset()}>
         Try Again
       </button>
     </div>
