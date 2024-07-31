a55d1d855aedf444c9978b2ded1becfc4c5e2c96
Tan28-art <tanmish03@gmail.com>
2024-07-31 14:05:26 +0300

    Added card component to represent movies on the home page

---
+++ starter/components/Card.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Image from "next/image";
+import * as React from "react";
+import { IoIosStarHalf } from "react-icons/io";
+
+export interface IAppProps {
+  key: string;
+  result: any;
+}
+
+export default function App({ result }: IAppProps) {
+  return (
+    <div className="group rounded-3xl border-2 p-3 sm:hover:shadow-emerald-200 sm:shadow-lg transition-shadow duration-200 dark:shadow-slate-700 dark:hover:shadow-emerald-500">
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={450}
+          height={300}
+          alt="/"
+          className='rounded-2xl group-hover:opacity-85 transition-opacity duration-300 '
+        ></Image>
+        <div className="">
+          <h2 className='font-bold truncate'>
+            {result.title || result.name}
+          </h2>
+          <p className='my-1 line-clamp-3 text-xs'>{result.overview}</p>
+          <p className='flex items-center gap-2'>
+            {result.release_date || result.first_air_date} 
+            <span className='text-lg'><IoIosStarHalf /></span>
+            {result.vote_average}
+          </p>
+          
+        </div>
+      </Link>
+    </div>
+  );
+}
--- starter/components/Results.tsx
+++ starter/components/Results.tsx
@@ -1,17 +1,15 @@
-import * as React from 'react';
+import * as React from "react";
+import Card from "./Card";
 
 export interface IAppProps {
-    results: any // will be the json object results passed as a prop to this component
+  results: any; // will be the json object results passed as a prop to this component
 }
 
-export default function Results ({results}: IAppProps) {
+export default function Results({ results }: IAppProps) {
   return (
-    <div>
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-8xl mx-auto gap-3 py-5 sm:px-5">
       {results.map((result: any) => (
-        <div key={result.id}>
-          <h1>{result.title}</h1>
-          
-        </div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
